451b08431376e91ccf8c8b9b586f454b78042bec
Илья <@DESKTOP-VPI29MR>
2024-10-08 16:46:01 +0200

    Добавьте файлы проекта.

---
+++ lab 1.2/Program.cs
@@ -0,0 +1,22 @@
+﻿
+try
+{
+    Console.WriteLine("Введите оценку по математике : ");
+    int m = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите оценку по физике: ");
+    int f = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите оценку по информатике: ");
+    int inf = int.Parse(Console.ReadLine());
+    if ((m == 4 && f == 4 && inf == 4) || (m == 5 && f == 5 && inf == 5))
+    {
+        Console.WriteLine("Студент получил степендию (ИСТИНА)");
+    }
+    else Console.WriteLine("Студент не получил степендию(ЛОЖ)");
+    if (m >= 6 || f >= 6 || m >= 6) Console.WriteLine("Ошибка ввода данных, введите верные данне от 2 до 5");
+    
+
+}
+catch
+{
+
+}
